264d1dedf95e91223f92c3f6241dac977c752957
Damilola OGUNGBENRO <73973314+dahmie-code@users.noreply.github.com>
2022-11-02 21:26:06 +0100

    Added better description in the test file

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,14 +2,12 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
 from flaskr import create_app
 from models import setup_db, Question, Category
 
-
+#  This class represents the trivia test case
 class TriviaTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
-
+   
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
@@ -22,6 +20,7 @@ class TriviaTestCase(unittest.TestCase):
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
+            
             # create all tables
             self.db.create_all()
     
@@ -29,11 +28,7 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
-
+ 
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
